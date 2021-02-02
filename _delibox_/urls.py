216007a6518eccdfb659e3bbfbcf3f49f14010aa
Andrzej Szomszor <andrzejszomszor@gmail.com>
2021-02-02 13:41:14 +0100

    Register feature creation

--- _delibox_/urls.py
+++ _delibox_/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name="login.html")),
     path('logout/', auth_views.LogoutView.as_view(next_page="/")),
+    path('register/', views.register_page),
     path('customer/', views.customer_page),
     path('courier/', views.courier_page,),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def register_page(request):
+    form = forms.RegisterForm()
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+            login (request, user)
+            return redirect('/')
+    return render(request, 'register.html', { 'form':form })
 
 
 @login_required()
